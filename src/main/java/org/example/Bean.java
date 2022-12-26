125cc8c1f3a16074f4e071f4c967dfc7c9312e22
laysstrongmax <laysstrongmax@gmail.com>
2022-12-26 11:16:07 +0300

    Initial commit

---
+++ src/main/java/org/example/Bean.java
@@ -0,0 +1,21 @@
+package org.example;
+import org.example.reflection.SomeInterface;
+import org.example.reflection.SomeOtherInterface;
+
+class SomeBean {
+
+    @AutoInjectable
+    private SomeInterface someField;
+    @AutoInjectable
+    private SomeOtherInterface otherField;
+
+    public SomeBean() {
+    }
+
+    public void go(){
+        someField.doSome();
+        otherField.doSome();
+    }
+
+
+}
